c50f29bc0bc5bef07a833e96e7fc854bd837924d
huymusic987 <huymarky05@gmail.com>
2025-09-20 17:14:32 +0700

    push

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -24,15 +24,6 @@ systemctl start docker
 
 systemctl start jenkins
 
-# Download the latest version of Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-
-# Make the downloaded file executable
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Confirm installation
-docker-compose --version
-
 echo "Jenkins, Docker installed"
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
